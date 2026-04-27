966799dccdb5654952898d746945898c1d0bfd73
theo-jeong-ops <theo.jeong.ops@gmail.com>
2026-04-27 14:09:56 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.post("/booking-complete", (req, res) => {
+  console.log("Booking received:", req.body);
+  res.send("ok");
+});
+
+app.get("/health", (req, res) => {
+  res.send("ok");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("running on " + PORT));
